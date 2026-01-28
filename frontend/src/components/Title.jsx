84158175713134d0842b192b5a895e791ccbcac0
mesanaullah <mdsanaullah1227@gmail.com>
2026-01-28 16:14:01 +0530

    title horizontal line is hide for small screens

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -6,7 +6,7 @@ const Title = ({ text1, text2 }) => {
     return (
         <div className='inline-flex gap-2 items-center mb-3'>
             <p className='text-gray-600'>{text1}<span className='text-[#2F2F2F] font-medium'>{text2}</span></p>
-            <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-[#2F2F2F]'></p>
+            <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-[#2F2F2F] hidden sm:block'></p>
         </div>
     )
 }
